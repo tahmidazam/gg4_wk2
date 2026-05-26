bd6edbf21c5405dbf30c3f51592ff1a95dbc99cd
Tahmid Azam <tahmid_azam@icloud.com>
2026-05-26 07:26:48 +0100

    fix: wildcard imports

--- src/gg4_wk2/__init__.py
+++ src/gg4_wk2/__init__.py
@@ -1,1 +1,68 @@
+from .condition import Condition
+from .condition_library import ConditionLibrary
+from .condition_library_illustrator import ConditionLibraryIllustrator
+from .estimator import BaseEstimator, EMUnknownInputEstimator, RandomEstimator
+from .evaluation_illustrator import EvaluationIllustrator
+from .evaluator import Evaluator
+from .model import LinearGaussianModel
+from .observations import Observations
+from .sampler import (
+    BaseSampler,
+    IntegerSampler,
+    LinearGaussianModelSampler,
+    WaveformSampler,
+)
+from .signal import Signal
+from .simulation import Simulation
+from .simulation_library import SimulationLibrary
+from .simulator import BaseSimulator, LinearGaussianSimulator
+from .validation import (
+    assert_2d,
+    assert_psd,
+    assert_shape,
+    assert_square,
+    assert_symmetric,
+)
+from .waveform import (
+    Constant,
+    Oscillation,
+    Pulse,
+    Ramp,
+    Waveform,
+    WaveformKind,
+    WhiteNoise,
+)
 
+__all__ = [
+    "Condition",
+    "ConditionLibrary",
+    "ConditionLibraryIllustrator",
+    "BaseEstimator",
+    "RandomEstimator",
+    "EMUnknownInputEstimator",
+    "EvaluationIllustrator",
+    "Evaluator",
+    "LinearGaussianModel",
+    "Observations",
+    "BaseSampler",
+    "IntegerSampler",
+    "LinearGaussianModelSampler",
+    "WaveformSampler",
+    "Signal",
+    "Simulation",
+    "SimulationLibrary",
+    "BaseSimulator",
+    "LinearGaussianSimulator",
+    "assert_2d",
+    "assert_psd",
+    "assert_shape",
+    "assert_square",
+    "assert_symmetric",
+    "Constant",
+    "Oscillation",
+    "Pulse",
+    "Ramp",
+    "Waveform",
+    "WaveformKind",
+    "WhiteNoise",
+]
